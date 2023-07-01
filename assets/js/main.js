ba3e15930e20f0efbcf02826aedf81acc056c8e4
omkar kadam <kadamom815@gmail.com>
2023-07-01 15:02:02 +0530

    Revert "updated"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
- (function($) {
+/*
+	Dimension by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
 
 	var	$window = $(window),
 		$body = $('body'),
